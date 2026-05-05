9be6995a84df59e9a8860b472bafc42b866feded
kalash choudhary <110890826+1kvitus@users.noreply.github.com>
2026-05-05 16:57:55 +0530

    Initial Commit

---
+++ AppleFrameWork/AppleFrameWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameWorkApp.swift
+//  AppleFrameWork
+//
+//  Created by Kalash Surendra Choudhary on 05/05/26.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
