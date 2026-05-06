495fbb3f17b5b9f61de5383cbb811b14a2ee7a66
kalash choudhary <110890826+1kvitus@users.noreply.github.com>
2026-05-06 17:41:30 +0530

    FRW-001
    Added Asset to the project

---
+++ AppleFrameWork/FrameWork.swift
@@ -0,0 +1,23 @@
+//
+//  FrameWork.swift
+//  AppleFrameWork
+//
+//  Created by Kalash Surendra Choudhary on 06/05/26.
+//
+
+import Foundation
+
+struct FrameWork {
+    var name: String
+    var imageName: String
+    var urlString: String
+    var description: String
+}
+
+
+struct MockData {
+    
+    static let data = [
+        FrameWork(name: <#T##String#>, imageName: <#T##String#>, urlString: <#T##String#>, description: <#T##String#>)
+    ]
+}
